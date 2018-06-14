1469ce0785cc92212d8f4719461fa634ece48c38
andgordio <andgordio@gmail.com>
2018-06-14 10:03:34 +0300

    Initial setup p I

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
+  <div class="max-w-md mx-auto p-8 mt-6 rounded-lg">
     <Home msg="Welcome to Your Vue.js App"/>
-    <i class="ion-md-heart"></i>
-    <p class="text-2xl text-red">HOLA</p>
+    <div class="flex py-2 items-baseline">
+      <div class="w-32 text-grey font-light">Name</div>
+      <div class="flex-1"><h1 class="text-5xl">Play 4 me <i class="ion-md-heart"></i></h1></div>
+    </div>
+    <div class="flex pt-6 pb-2 items-baseline">
+      <div class="w-32 text-grey font-light">Desciption</div>
+      <div class="flex-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit at omnis cupiditate animi ut libero sit nobis? Accusamus rerum corporis cupiditate architecto quisquam nihil, consectetur numquam sapiente maiores, nemo praesentium? </div>
+    </div>
+    <div class="flex pt-4 pb-2 items-baseline">
+      <div class="w-32 text-sm text-grey font-light">Platform</div>
+      <div class="flex-1">Desktop</div>
+    </div>
+    <div class="flex pt-4 pb-2 items-baseline">
+      <div class="w-32 text-sm text-grey font-light">Created by</div>
+      <div class="flex-1"><a class="no-underline text-blue" href="http://twitter.com/andgordy" target="_blank">And Gordy</a></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import '@/styles.css'
 import Home from '@/components/Home.vue'
 
 export default {
-  name: 'home',
   components: {
     Home
+  },
+  name: 'app',
+  data () {
+    return {
+    }
   }
 }
 </script>
+
+<style lang="scss">
+</style>
